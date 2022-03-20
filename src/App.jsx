239fd9e9158f6aa0233a7273c492c1b69f57916d
Zerowhizkey <marcus.piri@cmeducations.se>
2022-03-20 23:45:05 +0100

    styling a test on ks3 ramverk

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "rgb(128,0,128)",
+      dark: "rgb(128,0,175)",
+      light: "#ffcdcd",
+    },
+    secondary: {
+      main: 'rgb(255,255,255)',
+      dark: '#43ef71',
+      light: '#ffcdcd',
+    },
+  },
+});
+
+function App() {
+  return (
+  <>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Routes>
+        <Route  path="/" element={<Home />}/>
+        <Route path="/products" element={<Products />}/>
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
+    </Router>
+  </ThemeProvider>
+  </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,182 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { NavLink, Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+// const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            PAP
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {/* {pages.map((page) => ( */}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Grid
+                  container
+                  direction="column"
+                  justifyContent="space-around"
+                  alignItems="stretch"
+                >
+                  <Button variant="contained" component={NavLink} to="/" >
+                    Home
+                  </Button>
+                  <Button
+                    variant="contained"
+                    component={NavLink}
+                    to="/products"
+                  >
+                    Products
+                  </Button>
+                  <Button variant="contained" component={NavLink} to="/cart">
+                    Cart
+                  </Button>
+                </Grid>
+              </MenuItem>
+              {/* ))} */}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            PAP
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {/* {pages.map((page) => ( */}
+            <Button
+              // key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            ></Button>
+            {/* {page} */}
+            <Button variant="text" component={NavLink} to="/" color="secondary">
+              Home
+            </Button>
+            <Button
+              variant="text"
+              component={NavLink}
+              to="/products"
+              color="secondary"
+            >
+              Products
+            </Button>
+            <Button
+              variant="text"
+              component={NavLink}
+              to="/cart"
+              color="secondary"
+            >
+              Cart
+            </Button>
+            {/* ))} */}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://previews.123rf.com/images/kongvector/kongvector1707/kongvector170700182/81502024-super-cool-red-chili-character-cartoon.jpg"
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {/* Disabled the profil navbar items */}
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu} disabled>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
